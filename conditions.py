e513b453c6abba98caad8102c2fc8bf9a29e0a0d
bobbyb32 <60702471+bobbyb32@users.noreply.github.com>
2020-02-16 09:53:26 -0500

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,8 @@
+temp= input ("Please enter a temperature: ")
+temp= int(temp)
+if temp >= 70 :
+    print ("no jacket required")
+if temp >=70 :
+    print ("no jacket required")
+if temp <70 :
+    print ("Wear a jacket")
